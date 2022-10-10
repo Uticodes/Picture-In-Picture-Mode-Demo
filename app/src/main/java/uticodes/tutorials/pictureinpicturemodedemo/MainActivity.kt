08266b6791cd6041bbb3fee22684e252a3b49f0b
Utibe Etim <etim.utibe@gmail.com>
2022-10-10 17:37:28 +0100

    Complete project

--- app/src/main/java/uticodes/tutorials/pictureinpicturemodedemo/MainActivity.kt
+++ app/src/main/java/uticodes/tutorials/pictureinpicturemodedemo/MainActivity.kt
@@ -1,43 +1,116 @@
 package uticodes.tutorials.pictureinpicturemodedemo
 
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
+import android.widget.VideoView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toAndroidRect
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import uticodes.tutorials.pictureinpicturemodedemo.ui.theme.PictureInPictureModeDemoTheme
+import uticodes.tutorials.pictureinpicturemodedemo.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
+
+    class MyReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            println("Clicked on PIP action")
+        }
+    }
+
+    private val isPipSupported by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            packageManager.hasSystemFeature(
+                PackageManager.FEATURE_PICTURE_IN_PICTURE
+            )
+        } else {
+            false
+        }
+    }
+
+    private var videoViewBounds = Rect()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PictureInPictureModeDemoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                AndroidView(
+                    factory = {
+                        VideoView(it, null).apply {
+                            setVideoURI(Uri.parse("android.resource://$packageName/${R.raw.tomandjerry}"))
+                            start()
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Purple700)
+                        .onGloballyPositioned {
+                            videoViewBounds = it.boundsInWindow().toAndroidRect()
+                        }
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun updatedPipParams(): PictureInPictureParams? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PictureInPictureParams.Builder()
+                .setSourceRectHint(videoViewBounds)
+                .setAspectRatio(Rational(16, 9))
+                .setActions(
+                    listOf(
+                        RemoteAction(
+                            Icon.createWithResource(
+                                applicationContext,
+                                R.drawable.ic_library_add
+                            ),
+                            "Add to library",
+                            "Add to library",
+                            PendingIntent.getBroadcast(
+                                applicationContext,
+                                0,
+                                Intent(applicationContext, MyReceiver::class.java),
+                                PendingIntent.FLAG_IMMUTABLE
+                            )
+                        )
+                    )
+                )
+                .build()
+        } else null
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PictureInPictureModeDemoTheme {
-        Greeting("Android")
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if(!isPipSupported) {
+            return
+        }
+        updatedPipParams()?.let { params ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                enterPictureInPictureMode(params)
+            }
+        }
     }
 }
